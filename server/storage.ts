4bb1b7c8a4ed153914c5202ebf642b2019a932ab
Shakir Seyfullazade <askerzade135@gmail.com>
2026-09-25 13:59:56 +0400

    Store publication media sizes

--- server/storage.ts
+++ server/storage.ts
@@ -164,6 +164,7 @@ export interface PublicationJob {
   timeZone?: string;
   mediaName?: string;
   mediaType?: string;
+  mediaSize?: number;
   thumbnailName?: string;
   thumbnailType?: string;
   mediaObjectPath?: string;
