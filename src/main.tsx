cbb4be1f657bfafbe01f384b8669121d3ce46415
Shakir Seyfullazade <askerzade135@gmail.com>
2026-09-25 15:51:21 +0400

    Mount offline banner and root error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { I18nProvider } from './i18n';
+import { NetworkStatusBanner } from './components/NetworkStatusBanner';
+import { AppErrorBoundary } from './components/AppErrorBoundary';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <I18nProvider>
-      <App />
+      <NetworkStatusBanner />
+      <AppErrorBoundary>
+        <App />
+      </AppErrorBoundary>
     </I18nProvider>
   </StrictMode>,
 );
