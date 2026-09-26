47f05be15490de2a227e0fecdfe05d75ffb7578d
Shakir Seyfullazade <askerzade135@gmail.com>
2026-09-26 20:34:01 +0400

    Route public legal pages before authentication

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,23 @@ import './index.css';
 import { I18nProvider } from './i18n';
 import { NetworkStatusBanner } from './components/NetworkStatusBanner';
 import { installGlobalCrashFallback } from './components/AppErrorBoundary';
+import { isPublicLegalPath, PublicLegalPage } from './components/PublicLegalPage';
 
 installGlobalCrashFallback();
+
+const legalPage = isPublicLegalPath(window.location.pathname);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <I18nProvider>
-      <NetworkStatusBanner />
-      <App />
+      {legalPage ? (
+        <PublicLegalPage kind={legalPage} />
+      ) : (
+        <>
+          <NetworkStatusBanner />
+          <App />
+        </>
+      )}
     </I18nProvider>
   </StrictMode>,
 );
